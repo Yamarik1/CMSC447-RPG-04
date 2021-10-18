e6ee8c275ebed4b0fd124b2bdb6c61c6cbe5d974
Adam Yamarik <yamarik1@umbc.edu>
2021-10-18 12:02:03 -0400

    Updated the url file to allow for testing to be possible, and added a test to make sure the homepage returned the proper .html page.

--- RPGClass/homepage/urls.py
+++ RPGClass/homepage/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "homepage"
 urlpatterns = [
-    path('', views.homepage, name='Menu'),
+    path('', views.homepage, name='menu'),
     path('mainquest/', views.mainquest, name='mainquest'),
     path('sidequest/', views.sidequest, name='sidequest'),
     path('profile/', views.profile, name='profile'),
