12f6ab3d06394985be4a15dd5599d821cd6bb62d
Adam Yamarik <yamarik1@umbc.edu>
2021-10-27 00:06:30 -0400

    Created the start of functionality of a quest, represented by a single question. The Question can be answered, and will tell the user how many questions they got right.

--- RPGClass/homepage/models.py
+++ RPGClass/homepage/models.py
@@ -25,8 +25,18 @@ class Quest(models.Model):
     def getLives(self):
         return self._Num_lives
 
-    def setLives(self):
-        return self._Num_lives
+    def setLives(self, num):
+        self._Num_lives = num
+        return "Changed successfully"
+
+    def getRight(self):
+        return self._Correct_answers
+
+    def rightAnsChosen(self):
+        self._Correct_answers += 1
+
+    def setRight(self, numRight):
+        self._Correct_answers = numRight
 
     # Protected members
 
@@ -34,7 +44,47 @@ class Quest(models.Model):
     _Quest_name = models.CharField(max_length=200, default="N/A")
     _Quest_description = models.CharField(max_length=200, default="N/A")
     _Num_lives = models.IntegerField(default=0)
+    _Correct_answers = models.IntegerField(default=0)
 
     def __str__(self):
         msg = "This is Quest number:" + str(self.pk)
         return msg
+
+
+# If an admin wishes, they may create quests directly in the app. This is opposed to it being on some other software,
+# like BlackBoard
+class Question(models.Model):
+    quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
+
+    # Public members
+    def getQuestion(self):
+        return self._Question_text
+
+    def setQuestion(self, text):
+        self._Question_text = text
+        return "Question changed"
+
+    # Private members
+    _Question_text = models.CharField(max_length=200, default="N/A")
+
+
+class Choice(models.Model):
+    # Public members
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def getChoice(self):
+        return self._choice_text
+
+    def setChoice(self, text):
+        self._choice_text = text
+        return "Changed successfully"
+
+    def getCorrect(self):
+        return self._isCorrectChoice
+
+    def setCorrect(self, isAnswer):
+        self._isCorrectChoice = isAnswer
+        return "Correct answer chosen"
+
+    _choice_text = models.CharField(max_length=200, default="N/A")
+    _isCorrectChoice = models.BooleanField(default=False)
--- RPGClass/homepage/views.py
+++ RPGClass/homepage/views.py
@@ -1,15 +1,10 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
-
 from django.http import HttpResponse
-from django.shortcuts import render
 
+from .models import Question, Quest, Choice
 
 
 def homepage(request):
@@ -18,6 +13,41 @@ def homepage(request):
 
 def mainquest(request):
     return HttpResponse("Placeholder for the Main Quest page")
+
+
+class mainquestView(generic.DetailView):
+    template_name = 'homepage/mQuestView.html'
+    model = Quest
+
+
+class mQuestSpecific(generic.DetailView):
+    template_name = "homepage/question.html"
+    model = Quest
+
+
+def answer(request, quest_id, question_id):
+    quest = get_object_or_404(Quest, pk=quest_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'homepage/question.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+
+    if selected_choice.getCorrect():
+        quest.rightAnsChosen()
+        quest.save()
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('homepage:summary', args=(quest.id,)))
+
+
+def summary(request, quest_id):
+    quest = get_object_or_404(Quest, pk=quest_id)
+    return render(request, 'homepage/summary.html', {'quest': quest})
 
 
 def sidequest(request):
