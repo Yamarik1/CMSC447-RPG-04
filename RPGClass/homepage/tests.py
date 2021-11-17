abe7f50b2b49f0b645741f1d9b95c6efbb094734
Yamarik1 <91854566+Yamarik1@users.noreply.github.com>
2021-11-17 17:20:14 -0500

    Fixed a couple tests that stopped working because of the changes in the content on some of the pages.

--- RPGClass/homepage/tests.py
+++ RPGClass/homepage/tests.py
@@ -177,7 +177,7 @@ class QuestViewTest(TestCase):
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Click here to Start of the quest!")
+        self.assertContains(response, "Click here to Start the quest!")
 
     # Checks to see if a quest with type zero will give the right pages
     def test_type_quest_0(self):
@@ -209,6 +209,20 @@ class QuestViewTest(TestCase):
         quest = C.quest_set.create()
         quest.setAvailable(True)
         quest.setType(0)
+        quest.setXP(10)
+        quest.save()
+
+        url = reverse('homepage:summary', args=(C.id, quest.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Click here to go back to the course page")
+
+    def test_summary_page_type_1(self):
+        C = Course.objects.create()
+        quest = C.quest_set.create()
+        quest.setAvailable(True)
+        quest.setType(1)
+        quest.setLives(1)
         quest.setXP(10)
         quest.save()
 
@@ -305,93 +319,6 @@ class QuestionsViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Question")
         self.assertContains(response, "This Question does not have any choices")
-
-
-#hearts test but with a server
-class hearts_server(LiveServerTestCase):
-
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(username='test', password='12test12')
-        self.user.save()
-
-        #need geckodriver.exe to run firefox. haven't tested it on other browsers
-        self.browser = webdriver.Firefox(executable_path=geck_path)
-
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-        self.user.delete()
-
-    #test if you can actually navigate the server
-    def test_server(self):
-        #basically "127.0.0.1:8000/accounts/login/" but when using tests, server is not the same
-        self.browser.get("%s%s" % (self.live_server_url, reverse('accounts:login')))
-        #making sure we are at the right url with the title tab
-        assert 'Title' in self.browser.title
-
-    def test_hearts(self):
-        #testList = [0]
-        #testChoice = ["1", "2", "3"]
-        #quest = CreateQuest("Test1", 1, 3, testList, testChoice)
-        #quest.setLives(3)
-
-        #hardcoded this from views since the create quest function couldn't reduce lives by 1
-        for newCourse in Course.objects.all():
-            newCourse.delete()
-
-        newCourse = Course.objects.create(pk=1)
-        newCourse.setName("Kaer Morhen")
-        newCourse.setSection(1)
-        newCourse.setMaxXP(5)
-
-        # Create custom quests with some test values
-        # Test Quest 1: using type 1 to give the user questions to answer
-        Q = newCourse.quest_set.create(pk=1)
-        Q.setName("Quest 1")
-        Q.setDesc("This is the first test quest")
-        Q.setLives(3)
-        Q.setAvailable(True)
-        Q.setType(1)
-
-        question = Q.question_set.create(pk=1)
-        question.setQuestion("What is the meaning of life")
-
-        c = question.choice_set.create()
-        c.setChoice("2B")
-        c.save()
-        c = question.choice_set.create()
-        c.setChoice("to Be")
-        c.save()
-        c = question.choice_set.create()
-        c.setChoice("to be dooby doo")
-        c.setCorrect(True)
-        c.save()
-
-        question.save()
-        Q.save()
-
-        newCourse.save()
-
-        #to get past homepage
-        self.client.login(username='test', password='12test12')
-
-        self.browser.get("%s%s" % (self.live_server_url, reverse('homepage:mQuest', args=(newCourse.id, Q.id,))))
-        assert 'quest' in self.browser.title
-
-        #finds the button when clicking on a multiple choice answer so we can click it
-        radio_button = self.browser.find_element(By.XPATH, "//input[@id='choice1']")
-        radio_button.click()
-        #get's the submit button so we click it and activate the answer view so that hearts can go down
-        element = self.browser.find_element(By.XPATH, "//input[@type='submit']")
-        element.click()
-
-        #since the quest we saved in this function is local copy, need the copy that we used to save the new amount of lives in
-        quest = get_object_or_404(Quest, pk=Q.id)
-
-        #originally had 3 lives and should be reduced to 2
-        self.assertEqual(quest.getLives(), 2)
-
 
 #testing number of lives
 class hearts(TestCase):
@@ -533,4 +460,22 @@ class courseTests(TestCase):
         self.assertContains(response, "Current Level: 3")
         self.assertContains(response, "Total XP earned: 29")
         self.assertContains(response, "Xp to next level: 1")
+
+
+# Add additional tests for the side quests
+class sideQuest(TestCase):
+
+    # Makes sure the side quest page exists
+    def test_sidequest_page(self):
+        course = Course.objects.create()
+        course.save()
+
+        url = reverse('homepage:sidequest', args=(1,))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Side Quests")
+
+
+
 
